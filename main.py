c874fb83dee4b9de62266e3c0daa7785ffa01721
dhruv50ae <linkdp2012@gmail.com>
2022-07-27 21:02:04 +0100

    Fetch time of day

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from tkinter import *
 import requests
+from datetime import datetime
 
+NOT_MY_LAT = 51.510357
+NOT_MY_LNG = -0.116773
 
-def get_quote():
-    response = requests.get(url="https://api.kanye.rest/")
-    response.raise_for_status()
-    quote = response.json()["quote"]
-    canvas.itemconfig(quote_text, text=quote)
+parameters = {
+    "lat": NOT_MY_LAT,
+    "lng": NOT_MY_LNG,
+    "formatted": 0,
+}
 
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
-                                fill="white")
-canvas.grid(row=0, column=0)
+print(sunrise)
+print(sunset)
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote())
-kanye_button.grid(row=1, column=0)
+timeNow = datetime.now()
 
-window.mainloop()
+print(timeNow.hour)
