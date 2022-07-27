1b6c73b7272a2967683cc22c45376f23496b14da
dhruv50ae <linkdp2012@gmail.com>
2022-07-27 21:10:37 +0100

    Final Project Complete

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-NOT_MY_LAT = 51.510357
-NOT_MY_LNG = -0.116773
+MY_EMAIL = "andrew.thatcher.50ae@gmail.com"
+MY_PASSWORD = "4qv(KC3bxn6*u("
+MY_LAT = 51.510357  # Your latitude
+MY_LONG = -0.116773  # Your longitude
 
-parameters = {
-    "lat": NOT_MY_LAT,
-    "lng": NOT_MY_LNG,
-    "formatted": 0,
-}
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-print(sunrise)
-print(sunset)
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
 
-timeNow = datetime.now()
 
-print(timeNow.hour)
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("andrew.thatcher.50ae@gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+        )
